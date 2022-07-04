1f36bc6603ce080a3341140e2cecc1895b4be8a7
olamiji-8 <olamijisotomi@gmail.com>
2022-07-04 13:18:10 +0100

    commit

--- src/Components/Home/Header/BlogList/BlogItem/index.js
+++ src/Components/Home/Header/BlogList/BlogItem/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Chip from '../../../../Common/Chip'
 import './style.css'
+import{Link} from 'react-router-dom'
 
 const BlogItem = ({blog :{id,description,title,createdAt,authorName,authorAvatar,category,cover}}) => {
   return (
     <div className='blogItem-wrap'>
-        <img src={cover} alt='cover'/>
+        <img src={cover} alt='cover' className='blogItem-cover'/>
+        <Chip label={category}/>
+        <h3>{title}</h3>
+        <p className='blogItem-desc'>{description}</p>
+
+        <footer>
+            <div className='blogItem-author'>
+                <img src={authorAvatar}alt='avatar'/>
+                <div>
+                    <h6>{authorName}</h6>
+                    <p>{createdAt}</p>
+                </div>
+
+            </div>
+            <Link className='blogItem-link' to ={'./blog/ $ id' } >=> </Link>
+        </footer>
     </div>
   )
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../Components/Home/Header'
 import BlogList from '../../Components/Home/Header/BlogList'
 import SearchBar from '../../Components/Home/Header/SearchBar'
 import { blogList } from '../../config/data'
 
 const Home = () => {
+    const [blogs, setBlogs] = useState(blogList);
+    const [searchKey, setsearchKey] = useState('')
+    // Search Submit
+
+    const handleSearchSubmit=event=>{
+        event.preventDefault();
+        handleSearchResults()
+    }
+    const handleSearchResults=()=>{
+        const allBlogs=blogList;
+        constfilteredBlogs=allBlogs.filter(blog=>blog.category.toLowerCase().includes(searchKey.toLocaleLowerCase().trim()));
+        setBlogs(filteredBlogs)
+    }
+    const handleClearSearch=() =>{
+        setBlogs(blogList);
+        setsearchKey('');
+    }
   return (
     <div>
     {/* Page Header */}
     <Header/>
 
     {/* Search Bar */}
-    <SearchBar/>
+    <SearchBar value={searchKey}
+    clearSearch={handleClearSearch}
+     formSubmit={handleSearchSubmit} handleSearchkey ={e=>setsearchKey(e.target.value)}/>
 
 
     {/* Blog List & Empty List */}
