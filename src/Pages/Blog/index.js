8d5158e54ad26fb97ae909b1bf2ef20b742ef19d
olamiji-8 <olamijisotomi@gmail.com>
2022-08-21 22:06:25 +0100

    update

--- src/Pages/Blog/index.js
+++ src/Pages/Blog/index.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { blogList } from '../../config/data';
+// import React, { useEffect, useState } from 'react'
+// import { blogList } from '../../config/data';
 import {Link} from 'react-router-dom'
 
 const Blog = () => {
     // const {id}=useParams();
-    const {blog, setBlog} = useState(null)
-    useEffect(() =>{
-        let blog=blogList.find((blog) =>blog.id ===parseInt(id));
-        if(blog){
-            setBlog(blog);
-        }
-    }, []);
+    // const {blog, setBlog} = useState(null)
+    // useEffect(() =>{
+    //     let blog=blogList.find((blog) =>blog.id ===parseInt(id));
+    //     if(blog){
+    //         setBlog(blog);
+    //     }
+    // }, []);
   return (
     <div>
      <Link to={'/'}>
